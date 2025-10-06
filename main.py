47266e75b4aecef565e92654040cfed323f5a0ce
Marcojaco <marcojacobborges26@gmail.com>
2025-10-06 11:59:43 -0300

    fix: ajustando porta

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import os
 
 app = Flask(__name__)
 app.secret_key = "trojan"  
@@ -37,5 +38,7 @@ def logout():
     session.pop('logado', None)  
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
